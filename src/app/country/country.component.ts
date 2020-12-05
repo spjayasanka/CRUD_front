d6bf19b4d78957fee5ddfff2c8bfb3c433c75b1d
sanjeewa <sanjeewapriyanath6@gmail.com>
2020-12-05 16:01:16 +0530

    firsts

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -10,7 +10,7 @@ import {Router} from '@angular/router';
 })
 export class CountryComponent implements OnInit {
 
-  countries: Country = [];
+  countries: Country;
 
   selectedCountry: Country = new Country();
   constructor(private countryService: CountryServiceService, private router: Router) { }
